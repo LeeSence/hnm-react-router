89035c1a5f4c49be9d862d387ad06f0493e693a6
JongHo Lee <jongho89822@gmail.com>
2024-01-11 21:33:01 +0900

    second commit

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const { id } = useParams();
   const getProductDetail = async () => {
     setLoading(true);
-    let url = `http://localhost:5000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/LeeSence/hnm-react-router/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setLoading(false);
